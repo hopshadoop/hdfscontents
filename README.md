d624395c8fb3d1752f90b172a6c63260254ddecf
karamel <jdowling@sics.se>
2017-02-22 16:59:45 +0100

    first version

---
+++ README.md
@@ -0,0 +1,1 @@
+Hdfs Connector for Jupyter Notebook.
---
+++ setup.py
@@ -0,0 +1,13 @@
+
+setup(
+  name = 'hdfscontents',
+  packages = ['hdfscontents'], 
+  version = '0.1',
+  description = 'A HDFS contents plugin for Jupyter Notebook',
+  author = 'Jim Dowling',
+  author_email = 'jim@logicalclocks.com',
+  url = 'https://github.com/hopshadoop/hdfscontents', 
+  download_url = 'https://github.com/hopshadoop/hdfscontents/tarball/0.1', 
+  keywords = ['hops', 'hopsworks', 'hdfs', 'jupyter'], # arbitrary keywords
+  classifiers = [],
+)
