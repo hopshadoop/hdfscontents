a906067ec1fab9e79b632aa039b8f92db79905e3
robzor <robin.eric.andersson@gmail.com>
2021-03-03 14:41:54 +0100

    Release 0.8 (#10)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name='hdfscontents',
-    version='0.7',
+    version='0.8',
     author='Ahmad Al-Shishtawy',
     author_email='alshishtawy@gmail.com',
     description='Jupyter content manager that uses the HDFS filesystem',
